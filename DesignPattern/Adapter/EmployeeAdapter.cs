0004bdcb1a4f3bf55e947658b37daf296cdbf4a8
Marchi30847 <s30847@pjwstk.edu.pl>
2025-11-29 00:06:54 +0100

    Second task completed

--- DesignPattern/Adapter/EmployeeAdapter.cs
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -1,13 +1,26 @@
-﻿// In case you need some guidance: https://refactoring.guru/design-patterns/adapter
-namespace DesignPattern.Adapter
+﻿namespace DesignPattern.Adapter
 {
     public class EmployeeAdapter : ITarget
     {
-        private readonly BillingSystem thirdPartyBillingSystem = new();
+        private readonly BillingSystem _thirdPartyBillingSystem = new();
 
-        //public void ProcessCompanySalary(...)
-        //{
-        //    ...
-        //}
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            List<Employee> employeeList = new List<Employee>();
+            
+            for (int i = 0; i < employeesArray.GetLength(0); i++)
+            {
+                int id = int.Parse(employeesArray[i, 0]);
+                
+                string name = employeesArray[i, 1];
+                string designation = employeesArray[i, 2];
+                
+                decimal salary = decimal.Parse(employeesArray[i, 3]);
+
+                employeeList.Add(new Employee(id, name, designation, salary));
+            }
+            
+            _thirdPartyBillingSystem.ProcessSalary(employeeList);
+        }
     }
 }
